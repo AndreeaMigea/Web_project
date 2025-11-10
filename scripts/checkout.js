adb46604ced22dfbe39f61276ddf09189b7dc9a2
AndreeaMigea <87006978+AndreeaMigea@users.noreply.github.com>
2025-11-10 11:42:28 +0200

    remove product from cart using dom

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -19,7 +19,8 @@ cart.forEach((cartItem) => {
   });
 
   cartSumaryHTML+= `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+          js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -112,5 +113,8 @@ document.querySelector('.js-order-summary')
         const productId = link.dataset.productId;
         removeFromCart(productId);
         console.log(cart);
+
+       const container =  document.querySelector(`.js-cart-item-container-${productId}`);
+       container.remove();
       });
     });
